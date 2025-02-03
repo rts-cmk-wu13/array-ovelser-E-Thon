2d55a61070d6b5705c3b9e907afd3165eb68b611
Emilie Thon <emilie.m.thon@gmail.com>
2025-02-03 09:21:27 +0100

    opgave 7: solved

--- arrays.js
+++ arrays.js
@@ -32,47 +32,42 @@ document.addEventListener("DOMContentLoaded", function () {
   // --- opgave 1 ---
   let newNumbers = numbersArray.filter((numbers) => numbers > 20);
   console.log("Opgave 1", newNumbers);
-  
 
   // --- opgave 2 ---
   let newCatsArray = catsArray.push("Sniffles");
   console.log("Opgave 2; length:", newCatsArray);
   console.log("Opgave 2; catsArray:", catsArray);
 
-
   // --- opgave 3 ---
   let trixie = dogsArray.includes("Trixie");
   console.log("Opgave 3a", trixie);
 
-  
   let baxter = dogsArray.includes("Baxter");
   console.log("Opgave 3b", baxter);
-
 
   // --- opgave 4 ---
   let bagheera = catsArray.find((cat) => cat === "Bagheera");
   console.log("Opgave 4a", bagheera);
-  
+
   let salem = catsArray.find((cat) => cat === "Salem");
   console.log("Opgave 4b", salem);
 
-
   // --- opgave 5 ---
-  let newNumbersArray = numbersArray.map((number) => number*3);
+  let newNumbersArray = numbersArray.map((number) => number * 3);
   console.log("Opgave 5", newNumbersArray);
-
 
   // --- opgave 6 ---
   let dogsArrayOneString = dogsArray.join("");
   console.log("Opgave 6", dogsArrayOneString);
 
   // --- opgave 7 ---
-
-  console.log("Opgave 7", new7);
+  let mango = fruitsArray.indexOf("Mango");
+  console.log("Opgave 7a", mango);
+  
+  let blaabaer = fruitsArray.indexOf("Blåbær");  
+  console.log("Opgave 7b", blaabaer);
 
   // --- opgave 8 ---
-
+  
   console.log("Opgave 8", new8);
-
-
 }); // ends DOMContentLoaded
