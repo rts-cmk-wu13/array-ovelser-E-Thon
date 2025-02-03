b6e5b2d7546a26870bbc4bc0cbf30493913591b4
Emilie Thon <emilie.m.thon@gmail.com>
2025-02-03 08:47:25 +0100

    Opgave 1 løst

--- arrays.js
+++ arrays.js
@@ -1,9 +1,59 @@
-document.addEventListener("DOMContentLoaded", function() {
-    let catsArray = ["Ollie", "Sophie", "Salem", "Tiger", "Binx", "Pumpkin", "Penny", "Lenny"];
-    let dogsArray = ["Barney", "Molly", "Baxter", "Polly", "Buddy", "Suki", "Watson"];
-    let fruitsArray = ["Banana", "Orange", "Pineapple", "Mango", "Plum", "Apricot"]
-    let numbersArray = [12, 4, 45, 17, 2, 26, 7, 55, 48, 32, 21]
+document.addEventListener("DOMContentLoaded", function () {
+  let catsArray = [
+    "Ollie",
+    "Sophie",
+    "Salem",
+    "Tiger",
+    "Binx",
+    "Pumpkin",
+    "Penny",
+    "Lenny",
+  ];
+  let dogsArray = [
+    "Barney",
+    "Molly",
+    "Baxter",
+    "Polly",
+    "Buddy",
+    "Suki",
+    "Watson",
+  ];
+  let fruitsArray = [
+    "Banana",
+    "Orange",
+    "Pineapple",
+    "Mango",
+    "Plum",
+    "Apricot",
+  ];
+  let numbersArray = [12, 4, 45, 17, 2, 26, 7, 55, 48, 32, 21];
 
-    // --- Løs opgaverne herunder --- 
+  // --- Løs opgaverne herunder ---
+  // --- opgave 1 ---
+  let newNumbers = numbersArray.filter((numbers) => numbers > 20);
+  
+  console.log("Opgave 1", newNumbers);
+  
 
-}) // ends DOMContentLoaded
+  // --- opgave 2 ---
+
+
+  // --- opgave 3 ---
+
+
+  // --- opgave 4 ---
+
+
+  // --- opgave 5 ---
+
+
+  // --- opgave 6 ---
+
+
+  // --- opgave 7 ---
+
+
+  // --- opgave 8 ---
+
+
+}); // ends DOMContentLoaded
